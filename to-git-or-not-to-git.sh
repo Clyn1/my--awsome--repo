979c8722a416fc6ecbe4bb34c134a57c9bd74a24
clinton <clintobhris@gmail.com>
2023-10-25 10:26:59 +0300

    clyn

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,5 @@
-curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jp ' . [] | select(.id==170).name'
+curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq ' . [] | select(.id==170).name'
+curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq ' . [] | select(.id==170).powerstats | .power'
+curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq ' . [] | select(.id==170).appearance | .gender'
+
+
