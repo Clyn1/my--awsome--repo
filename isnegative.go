e0f12d980c6e198869ecec7b441e284eb6772da7
clinton <clintobhris@gmail.com>
2023-10-26 18:09:48 +0300

    addneg

--- isnegative.go
+++ isnegative.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		z01.PrintRUne('T')
+		z01.PrintRune('T')
 	} else {
-		z01.PrintRUne('F')
+		z01.PrintRune('F')
 	}
-	z01.PrintRUne('\n')
+	z01.PrintRune('\n')
 }
