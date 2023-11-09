19eeacb72958d50fb5132522ff33eeccf9fd93d6
cliochieng <ochiengclyntono@gmail.com>
2023-11-09 18:25:13 +0300

    s

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,34 +1,11 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-	"strconv"
-	"strings"
-)
+func StringToIntSlice(str string) []int {
+	var result []int
 
-func StringToIntSlice(input string) ([]int, error) {
-	strNums := strings.Fields(input)
-
-	var intSlice []int
-	for _, strNum := range strNums {
-		num, err := strconv.Atoi(strNum)
-		if err != nil {
-			return nil, err
-		}
-		intSlice = append(intSlice, num)
+	for _, char := range str {
+		result = append(result, int(char))
 	}
 
-	return intSlice, nil
-}
-
-func (piscine) {
-	input := "1 2 3 4 5"
-	intSlice, err := StringToIntSlice(input)
-	if err != nil {
-		z01.PrintRune("Error:", err)
-		return
-	}
-
-	z01.PrintRune("Input String:", input)
-	z01.PrintRune("Converted Int Slice:", intSlice)
+	return result
 }
