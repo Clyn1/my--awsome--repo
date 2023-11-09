0b43207a5e4995c0d058ee96f9f04a33c52ff768
cliochieng <ochiengclyntono@gmail.com>
2023-11-09 11:21:03 +0300

    g

--- fooddeliverytime.go
+++ fooddeliverytime.go
@@ -1,35 +1,17 @@
 package piscine
 
-import (
-	"fmt"
-)
-
-type menu struct {
-	burger  int
-	chips   int
-	nuggets int
+type food struct {
+	preptime int
 }
 
-func FoodDeliveryTime(order string) (int, error) {
-	m := menu{15, 10, 12}
-	switch order {
-	case "burger":
-		return m.burger, nil
-	case "chips":
-		return m.chips, nil
-	case "nuggets":
-		return m.nuggets, nil
-	default:
-		return 0, fmt.Errorf("404")
+func FoodDeliveryTime(oder string) int {
+	menu := map[string]food{
+		"burger":  {preptime: 15},
+		"chips":   {preptime: 10},
+		"nuggets": {preptime: 12},
 	}
-}
-
-func main() {
-	order := "burger"
-	time, err := FoodDeliveryTime(order)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("The time it takes to prepare %s is %d minutes.\n", order, time)
+	if item, exists := menu[order]; exists {
+		return item.preptime
 	}
+	return 404
 }
