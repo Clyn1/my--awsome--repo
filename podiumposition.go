1b6eaf2c11c45ceae96d748027d5ad14f18d9710
onlyclyn <ochiengclyntono@gmail.com>
2023-11-10 01:19:30 +0300

    d

--- podiumposition.go
+++ podiumposition.go
@@ -1,35 +1,15 @@
 package piscine
 
-import (
-	"fmt"
-	"sort"
-)
-
-func PodiumPosition(competitors [][]string) [][]string {
-	podium := make([][]string, 3)
-	for i := 0; i < 3; i++ {
-		podium[i] = competitors[i]
+func PodiumPosition(podium [][]string) [][]string {
+	if len(podium) == 0 {
+		return [][]string{}
 	}
-	sort.Slice(podium, func(i, j int) bool {
-		return podium[i][1] < podium[j][1]
-	})
+	for i := 0; i < len(podium)-1; i++ {
+		for j := 0; j < len(podium)-1-i; j++ {
+			if len(podium[j]) > len(podium[j+1]) {
+				podium[j], podium[j+1] = podium[j+1], podium[j]
+			}
+		}
+	}
 	return podium
-}
-
-func main() {
-	competitors := [][]string{
-		{"Lewis Hamilton", "1"},
-		{"Max Verstappen", "2"},
-		{"Sergio Perez", "3"},
-		{"Valtteri Bottas", "4"},
-		{"Charles Leclerc", "5"},
-		{"Lando Norris", "6"},
-		{"Carlos Sainz", "7"},
-		{"Daniel Ricciardo", "8"},
-		{"Sebastian Vettel", "9"},
-		{"Fernando Alonso", "10"},
-	}
-	fmt.Println("Original competitors:", competitors)
-	podium := PodiumPosition(competitors)
-	fmt.Println("Podium positions:", podium)
 }
