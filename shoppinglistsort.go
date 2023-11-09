17d0563707bddb9a22abb4a56a4388c07b077cd9
onlyclyn <ochiengclyntono@gmail.com>
2023-11-09 22:27:18 +0300

    d

--- shoppinglistsort.go
+++ shoppinglistsort.go
@@ -1,30 +1,10 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
-type ByLength []string
-
-func (s ByLength) Len() int           { return len(s) }
-func (s ByLength) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
-func (s ByLength) Less(i, j int) bool { return len(s[i]) < len(s[j]) }
-
-func ShoppingListSort(items []string) []string {
-	sortedItems := make([]string, len(items))
-	copy(sortedItems, items)
-	Sort(ByLength(sortedItems))
-	return sortedItems
-}
-
-func main() {
-	shoppingList := []string{"eggs", "bread", "milk", "apple", "cheese"}
-	sortedList := ShoppingListSort(shoppingList)
-
-	for _, item := range sortedList {
-		for _, char := range item {
-			z01.PrintRune(char)
+func ShoppingListSort(slice []string) []string {
+	for i := 0; i < len(slice)-1; i++ {
+		if len(slice[i]) > len(slice[i+1]) {
+			slice[i], slice[i+1] = slice[i+1], slice[i]
 		}
-		z01.PrintRune('\n')
 	}
+	return slice
 }
