836e9b258dd95c4d396fc73f2b91e63048d740c5
cliochieng <ochiengclyntono@gmail.com>
2023-11-09 15:52:30 +0300

    s

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -3,24 +3,13 @@ package piscine
 import "fmt"
 
 func DealAPackOfCards(deck []int) {
-	if len(deck)%4 != 0 {
-		fmt.Println("Cannot deal the cards evenly among 4 players.")
-		return
+	for i := 0; i < 4; i++ {
+		fmt.Printf("Player %d: ", i+1)
+		for j := i; j < len(deck); j += 4 {
+			fmt.Printf("%d, ", deck[j])
+		}
+		fmt.Println()
 	}
-
-	cardsPerPlayer := len(deck) / 4
-
-	fmt.Println("Player 1:")
-	fmt.Println(deck[:cardsPerPlayer])
-
-	fmt.Println("\nPlayer 2:")
-	fmt.Println(deck[cardsPerPlayer : 2*cardsPerPlayer])
-
-	fmt.Println("\nPlayer 3:")
-	fmt.Println(deck[2*cardsPerPlayer : 3*cardsPerPlayer])
-
-	fmt.Println("\nPlayer 4:")
-	fmt.Println(deck[3*cardsPerPlayer:])
 }
 
 func main() {
