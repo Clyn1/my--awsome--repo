896478eb684158bc8b1f086790322c1e5ba873ed
cliochieng <ochiengclyntono@gmail.com>
2023-11-14 09:38:43 +0300

    ws

--- listclear.go
+++ listclear.go
@@ -1,47 +1,8 @@
-package piscine
+package main
 
-
-type Data interface{}package main
-
+import "fmt"
 
 type Data interface{}
-
-type Node struct {
-	data Data
-	next *Node
-}
-
-type LinkedList struct {
-	head *Node
-}
-
-func ListClear(l *LinkedList) {
-	l.head = nil
-}
-
-func main() {
-	list := LinkedList{}
-	list.head = &Node{data: 1}
-	list.head.next = &Node{data: 2}
-	list.head.next.next = &Node{data: 3}
-
-	fmt.Println("Before ListClear:")
-	printList(list)
-
-	ListClear(&list)
-
-	fmt.Println("After ListClear:")
-	printList(list)
-}
-
-func printList(l LinkedList) {
-	current := l.head
-	for current != nil {
-		fmt.Print(current.data, " ")
-		current = current.next
-	}
-	fmt.Println()
-}
 
 type Node struct {
 	data Data
