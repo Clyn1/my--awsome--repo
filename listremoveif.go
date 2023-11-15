75e0635c7f8364c6f37f40a81d54adb8f7f70727
cliochieng <ochiengclyntono@gmail.com>
2023-11-15 13:30:49 +0300

    d

--- listremoveif.go
+++ listremoveif.go
@@ -1,19 +1,8 @@
 package piscine
 
-func ListMerge(l1, l2 *List) {
-	if l1 == nil || l2 == nil {
-		return
+func ListMerge(l1 *List, l2 *List) {
+	for l2.Head != nil {
+		ListPushBack(l1, l2.Head.Data)
+		l2.Head = l2.Head.Next
 	}
-
-	if l1.Head == nil {
-		l1.Head = l2.Head
-		l1.Tail = l2.Head
-		return
-	}
-
-	current := l1.Head
-	for current.Next != nil {
-		current = current.Next
-	}
-	current.Next = l2.Head
 }
