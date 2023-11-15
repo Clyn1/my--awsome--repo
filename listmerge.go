4b2fb1e92b47954d7c0b580e0f6d5c0e335806de
cliochieng <ochiengclyntono@gmail.com>
2023-11-15 16:35:33 +0300

    clyn

--- listmerge.go
+++ listmerge.go
@@ -1,7 +1,7 @@
 package piscine
 
 // ListPushBack inserts a new element at the end of the list
-func ListPushBack(l *List, data int) {
+func ListPushBack(l *List, data interface{}) {
 	newNode := &NodeL{Data: data, Next: nil}
 
 	if l.Head == nil {
@@ -20,7 +20,15 @@ func ListPushBack(l *List, data int) {
 // ListMerge merges two lists by appending elements from l2 to l1
 func ListMerge(l1 *List, l2 *List) {
 	for l2.Head != nil {
-		ListPushBack(l1, l2.Head.Data)
+		// Type assertion to convert interface{} to int
+		data, ok := l2.Head.Data.(int)
+		if !ok {
+			// Handle the case where the assertion fails
+			// For now, we'll assume the data is always an int
+			panic("Data is not of type int")
+		}
+
+		ListPushBack(l1, data)
 		l2.Head = l2.Head.Next
 	}
 }
