9b4747499970bfef20eaaf223b2a38f8b351f97b
cliochieng <ochiengclyntono@gmail.com>
2023-11-15 15:52:01 +0300

    s

--- listmerge.go
+++ listmerge.go
@@ -10,25 +10,3 @@ type Node struct {
 type LinkedList struct {
 	head *Node
 }
-
-func ListMerge(l1 *LinkedList, l2 *LinkedList) {
-	for l2.head != nil {
-		ListPushBack(l1, l2.head.data)
-		l2.head = l2.head.next
-	}
-}
-
-func ListPushBack(l *LinkedList, data Data) {
-	newNode := &Node{data: data}
-	if l.head == nil {
-		l.head = newNode
-		return
-	}
-
-	current := l.head
-	for current.next != nil {
-		current = current.next
-	}
-
-	current.next = newNode
-}
