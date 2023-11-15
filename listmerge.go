7c3b14fc97694aefb2232539d82da19a141d80ce
cliochieng <ochiengclyntono@gmail.com>
2023-11-15 12:48:36 +0300

    d

--- listmerge.go
+++ listmerge.go
@@ -1,33 +1,53 @@
 package piscine
 
+import "fmt"
+
+type Data interface{}
+
 type NodeL struct {
-	Data int
+	Data Data
 	Next *NodeL
 }
 
-type List struct {
+type LinkedList struct {
 	Head *NodeL
 }
 
-func ListPushBack(l *List, data int) {
-	newNode := &NodeL{Data: data, Next: nil}
-
-	if l.Head == nil {
-		l.Head = newNode
-		return
+func ListAt(l *LinkedList, pos int) *NodeL {
+	if l == nil || l.Head == nil || pos < 0 {
+		return nil
 	}
 
 	current := l.Head
-	for current.Next != nil {
+	for i := 0; i < pos; i++ {
+		if current.Next == nil {
+			return nil
+		}
 		current = current.Next
 	}
 
-	current.Next = newNode
+	return current
 }
 
-func ListMerge(l1 *List, l2 *List) {
-	for l2.Head != nil {
-		ListPushBack(l1, l2.Head.Data)
-		l2.Head = l2.Head.Next
+func main() {
+	list := LinkedList{}
+	list.Head = &NodeL{Data: 1}
+	list.Head.Next = &NodeL{Data: 2}
+	list.Head.Next.Next = &NodeL{Data: 3}
+
+	pos1Node := ListAt(&list, 1)
+
+	if pos1Node != nil {
+		fmt.Println("Node at position 1:", pos1Node.Data)
+	} else {
+		fmt.Println("Position 1 is out of bounds.")
+	}
+
+	pos5Node := ListAt(&list, 5)
+
+	if pos5Node != nil {
+		fmt.Println("Node at position 5:", pos5Node.Data)
+	} else {
+		fmt.Println("Position 5 is out of bounds.")
 	}
 }
