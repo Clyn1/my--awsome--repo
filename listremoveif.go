86fbdeb58ef4123afb3972e3684f4ea3c7652895
cliochieng <ochiengclyntono@gmail.com>
2023-11-15 13:16:04 +0300

    d

--- listremoveif.go
+++ listremoveif.go
@@ -10,17 +10,3 @@ type Node struct {
 type LinkedList struct {
 	head *Node
 }
-
-func ListMerge(l1 *LinkedList, l2 *LinkedList) {
-	if l1.head == nil {
-		l1.head = l2.head
-		return
-	}
-
-	current := l1.head
-	for current.next != nil {
-		current = current.next
-	}
-
-	current.next = l2.head
-}
