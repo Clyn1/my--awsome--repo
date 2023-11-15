0f2b41ab1454fa9e576a19c0f5283e59c06511bc
cliochieng <ochiengclyntono@gmail.com>
2023-11-15 12:41:04 +0300

    d

--- listmerge.go
+++ listmerge.go
@@ -1,5 +1,30 @@
 package piscine
 
+type NodeL struct {
+	Data int
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+}
+
+func ListPushBack(l *List, data int) {
+	newNode := &NodeL{Data: data, Next: nil}
+
+	if l.Head == nil {
+		l.Head = newNode
+		return
+	}
+
+	current := l.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+
+	current.Next = newNode
+}
+
 func ListMerge(l1 *List, l2 *List) {
 	for l2.Head != nil {
 		ListPushBack(l1, l2.Head.Data)
