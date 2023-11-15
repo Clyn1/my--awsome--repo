0b836af9f4de42448eeb7631124ba0163b82654a
cliochieng <ochiengclyntono@gmail.com>
2023-11-15 15:42:24 +0300

    s

--- listmerge.go
+++ listmerge.go
@@ -1,8 +1,23 @@
 package piscine
 
-func ListMerge(l1 *List, l2 *List) {
-	for l2.Head != nil {
-		ListPushBack(l1, l2.Head.Data)
-		l2.Head = l2.Head.Next
+func ListMerge(l1 *LinkedList, l2 *LinkedList) {
+	for l2.head != nil {
+		ListPushBack(l1, l2.head.Data)
+		l2.head = l2.head.Next
 	}
+}
+
+func ListPushBack(l *LinkedList, data Data) {
+	newNode := &Node{data: data}
+	if l.head == nil {
+		l.head = newNode
+		return
+	}
+
+	current := l.head
+	for current.next != nil {
+		current = current.next
+	}
+
+	current.next = newNode
 }
