6e830dc465fde5d32e5cd28b2b1656311013d10a
cliochieng <ochiengclyntono@gmail.com>
2023-11-18 09:19:05 +0300

    cliochieng

--- btreeapplypreorder.go
+++ btreeapplypreorder.go
@@ -1,57 +1,10 @@
 package piscine
 
-type TreeNode struct {
-	Value int
-	Left  *TreeNode
-	Right *TreeNode
-}
-
-func ApplyPreorder(root *TreeNode, f func(int)) {
-	if root != nil {
-		f(root.Value)
-		ApplyPreorder(root.Left, f)
-		ApplyPreorder(root.Right, f)
+func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
 	}
-}
-
-func printNode(val int, buffer *[]byte) {
-	*buffer = append(*buffer, byte(val+'0'))
-}
-
-func printBuffer(buffer []byte) {
-	for _, char := range buffer {
-		print(char)
-	}
-}
-
-func main() {
-	root := &TreeNode{
-		Value: 1,
-		Left: &TreeNode{
-			Value: 2,
-			Left: &TreeNode{
-				Value: 3,
-			},
-			Right: &TreeNode{
-				Value: 4,
-			},
-		},
-		Right: &TreeNode{
-			Value: 5,
-			Left: &TreeNode{
-				Value: 6,
-			},
-			Right: &TreeNode{
-				Value: 7,
-			},
-		},
-	}
-
-	var outputBuffer []byte
-
-	ApplyPreorder(root, func(val int) {
-		printNode(val, &outputBuffer)
-	})
-
-	printBuffer(outputBuffer)
+	f(root.Data)
+	BTreeApplyPreorder(root.Left, f)
+	BTreeApplyPreorder(root.Right, f)
 }
