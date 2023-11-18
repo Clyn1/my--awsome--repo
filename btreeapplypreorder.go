0da79df890dc78507d3db1ea0289797ba08a2d58
cliochieng <ochiengclyntono@gmail.com>
2023-11-18 08:53:48 +0300

    clyn

---
+++ btreeapplypreorder.go
@@ -0,0 +1,52 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+type TreeNode struct {
+	Value int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func ApplyPreorder(root *TreeNode, f func(int)) {
+	if root != nil {
+		f(root.Value)
+		ApplyPreorder(root.Left, f)
+		ApplyPreorder(root.Right, f)
+	}
+}
+
+func printNode(val int) {
+	z01.PrintRune(rune(val))
+}
+
+func main() {
+	root := &TreeNode{
+		Value: 1,
+		Left: &TreeNode{
+			Value: 2,
+			Left: &TreeNode{
+				Value: 3,
+			},
+			Right: &TreeNode{
+				Value: 4,
+			},
+		},
+		Right: &TreeNode{
+			Value: 5,
+			Left: &TreeNode{
+				Value: 6,
+			},
+			Right: &TreeNode{
+				Value: 7,
+			},
+		},
+	}
+
+	ApplyPreorder(root, func(val int) {
+		printNode(val)
+	})
+	z01.PrintRune('\n')
+}
